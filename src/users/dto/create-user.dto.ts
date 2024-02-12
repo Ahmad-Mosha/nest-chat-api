dc9dc8b394f9cd03d0424b10f34491f59f2c7b5c
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2024-02-12 09:45:10 +0200

    (feat): swagger implementation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+
 export class CreateUserDto {
+	@IsNotEmpty()
+	@IsString()
+	@ApiProperty({
+		description: 'The name of the user',
+		type: String,
+		required: true,
+	})
 	name: string;
+
+	@IsNotEmpty()
+	@IsEmail()
+	@ApiProperty({
+		description: 'The email of the user',
+		type: String,
+		required: true,
+	})
 	email: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@Matches(/^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/, {
+		message:
+			'Password must be at least 8 characters long 1 lowercase letter, and 1 number',
+	})
+	@ApiProperty({
+		description: 'The password of the user',
+		type: String,
+		required: true,
+	})
 	password: string;
 }
