2cb5f6cc14ba745e496333f755452f380d56d166
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2024-02-14 15:59:35 +0200

    (fix): MongoDB URI exposed

--- src/sessions/session.config.ts
+++
@@ -1,6 +0,0 @@
-import { ConnectMongoOptions } from 'connect-mongo/build/main/lib/MongoStore';
-
-export default {
-	mongoUrl: 'mongodb+srv://Mosha:Mosha010@mosha.ntwsh2t.mongodb.net/',
-	dbName: 'chat-app',
-} as ConnectMongoOptions;
