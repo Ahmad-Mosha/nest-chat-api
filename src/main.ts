2da6cf2dc70f0958c5bab42b864655e61986b260
Ahmed <ahmedeihab709@yahoo.com>
2024-02-18 19:37:17 +0200

    (feat): enable cores for front-end usage & returning specific data from endpoints(oAuth(google - twitter) and local)

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ import { ExceptionLogger } from './exceptionFilters/logger.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [`${process.env.NEXT_API_URL}`],
+    credentials: true,
+  });
   app.use(
     session({
       name: 'chat-app',
